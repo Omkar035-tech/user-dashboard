73e417b09d19d4577d5b42587dfff59ba0b434ae
Omkar035-tech <omkarapandkar4u@gmail.com>
2024-12-05 03:12:14 +0530

    connected DB via Localhost Xampp

---
+++ backend/index.php
@@ -0,0 +1,38 @@
+<?php
+//main file for all routes
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+require_once 'config/database.php';
+require_once 'includes/functions.php';
+
+initializeDatabase();
+
+$request = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch (true) {
+    case preg_match('/\/api\/users\/?$/', $request):
+        switch ($method) {
+            case 'GET':
+                require 'api/getusers.php';
+                break;
+            case 'POST':
+                require 'api/adduser.php';
+                break;
+            case 'PUT':
+                require 'api/updateuser.php';
+                break;
+            case 'DELETE':
+                require 'api/deleteuser.php';
+                break;
+            default:
+                sendJsonResponse(['error' => 'Method not allowed'], 405);
+        }
+        break;
+
+    default:
+        sendJsonResponse(['error' => 'Endpoint not found'], 404);
+}
